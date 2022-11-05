1c80b1a99a99250e72728b340295bc1a17a4ec49
Lucas Alexandre Neves <lucaslhneves@gmail.com>
2022-11-05 15:17:02 -0300

    First I created a variable called frame to connect the back with the front. A variable was also created to define whose shift it is. And also a variable to know if the game is running or not.

---
+++ script.js
@@ -0,0 +1,13 @@
+// Initial Data
+let square = {
+    a1: '', a2: '', a3: '',
+    b1: '', b2: '', b3: '',
+    c1: '', c2: '', c3: ''
+}
+let player = ''
+let warning = ''
+let playing = false
+
+// Events
+
+// Functions
