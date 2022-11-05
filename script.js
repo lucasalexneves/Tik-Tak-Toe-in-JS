1d2954ff075a2b53df16649e3538928850cdcc8c
Lucas Alexandre Neves <lucaslhneves@gmail.com>
2022-11-05 16:23:07 -0300

    I created the variable checkGame(), checkWinnerFor() and isFull() to check each play if there was a winner

--- script.js
+++ script.js
@@ -12,16 +12,17 @@ reset()
 
 // Events
 document.querySelector('.reset').addEventListener('click', reset)
-document.querySelector('.item').forEach(item => {
+document.querySelectorAll('.item').forEach(item => {
     item.addEventListener('click', itemClick)
 });
 
 // Functions
 function itemClick(event){
     let item = event.target.getAttribute('data-item')
-    if(square[item] === ''){
+    if(playing && square[item] === ''){
         square[item] = player
         renderSquare()
+        togglePlayer()
     }
 }
 
@@ -30,7 +31,7 @@ function reset() {
 
     let random = Math.floor(Math.random() * 2)
     
-    player = (random === 0) ? 'x' : '0'
+    player = (random === 0) ? 'x' : 'o'
     /* if(random === 0){
         player = 'x'
     } else {
@@ -52,9 +53,70 @@ function renderSquare() {
         let item = document.querySelector(`div[data-item=${i}]`)  
         item.innerHTML = square[i]
     }
+
+    checkGame()
 }
 
 function renderInformation() {
     document.querySelector('.vez').innerHTML = player
     document.querySelector('.resultado').innerHTML = warning
+}
+
+function togglePlayer(){
+    
+    player = (player === 'x') ? 'o' : 'x'
+    /* if(player === 'x') {
+        player = 'o'
+    } else {
+        player = 'x'
+    }*/
+
+    renderInformation()
+}
+
+function checkGame() {
+    if(checkWinnerFor('x')){
+        warning = 'O "X" venceu'
+        playing = false
+    }else if(checkWinnerFor('o')){
+        warning = 'O "O" venceu'
+        playing = false
+    } else if(isFull()){
+        warning = 'Deu empate'
+        playing = false
+    }
+}
+
+function checkWinnerFor(player){
+    let pos = [
+        'a1,a2,a3',
+        'b1,b2,b3',
+        'c1,c2,c3',
+
+        'a1,b1,c1',
+        'a2,b2,c2',
+        'a3,b3,c3',
+
+        'a1,b2,c3',
+        'a3,b2,c1'
+    ]
+    for(let w in pos){
+        let pArray = pos[w].split(',') // a1, a2,   a3
+        let hasWon = pArray.every(option => square[option] === player)
+        if (hasWon){
+            return true
+        }
+    }
+
+    return false
+}
+
+function isFull(){
+    for(let i in square){
+        if(square[i] === ''){
+            return false
+        }
+    }
+
+    return true
 }
